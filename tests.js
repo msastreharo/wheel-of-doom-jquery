fa64e92fdfa759f9d36a4abf8a3bfbd915f60689
Javi <javi@jsrois.net>
2021-11-18 11:39:53 +0100

    Añadida infrastructura de test

---
+++ tests.js
@@ -0,0 +1,15 @@
+const chai = window.chai
+const expect = chai.expect
+
+describe('Wheel of Doom', () => {
+  it('test que tiene que fallar', () => {
+
+    
+    expect(true).to.equal(false);
+  })
+  it('test que tiene que pasar', () => {
+
+    
+    expect(true).to.equal(true);
+  })
+})
