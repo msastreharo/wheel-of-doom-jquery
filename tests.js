1d2d0765d5e035ae62073763f2597e56ce089162
msastreharo <msastreharo@gmail.com>
2021-11-19 11:26:56 +0100

    Testing started

---
+++ tests.js
@@ -0,0 +1,12 @@
+const chai = window.chai;
+const expect = chai.expect;
+
+describe("Wheel of Doom", () => {
+    it("El array de coders tiene 24 coders al principio", () => {
+        let coderArray = ["Alexia", "Alisa", "Ana C.", "Anna G.", "Candy", "Carmen", "Desirée", "Faby", "Gabrielle", "Gràcia", "Helen", "Joana", "Judith", "Kristina", "Laura C.", "Laura M.", "Marisa", "Rosa", "Sandra", "Sara", "Sonia", "Tamara", "Valentina", "Yuliya"];
+
+        expect(coderArray.length).to.equal(24);
+    })
+
+    it("Puedo elegir candidatas aleatoriamente y sacarlas de la lista", () )
+})
